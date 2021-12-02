d3b198ec00d2c0b7091ae4b13fb14637d6300fc7
rndtim <51507837+rndtim@users.noreply.github.com>
2021-12-02 14:58:27 +0100

    Start decomposition

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,100 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="main">
+    <form @submit.prevent>
+      <h4>Create post</h4>
+      <input
+          :value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Title">
+      <input
+          :value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Description">
+      <button
+          @click="createPost"
+          class="btn">
+        Create
+      </button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Title</strong> {{ post.title }}</div>
+      <div><strong>Description</strong> {{ post.body }}</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JS', body: "Description"},
+        {id: 2, title: 'JS 2', body: "Description 2"},
+        {id: 3, title: 'JS 3', body: "Description 3"},
+        {id: 3, title: 'JS 4', body: "Description 4"},
+      ],
+      title: '',
+      body: '',
+
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+
+      this.posts.push(newPost);
+
+      this.title = '';
+      this.body = '';
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 2px solid teal;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.main {
+  margin: 0 auto;
+  width: 75%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+  padding: 10px 15px;
+  border: 1px solid deepskyblue;
+  margin-top: 10px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 10px;
+  align-self: flex-start;
+  background-color: lightgreen;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 5px 2px 2px green;
 }
 </style>
